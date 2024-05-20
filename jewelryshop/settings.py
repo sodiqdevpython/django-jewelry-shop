85b1231bd863db30a17d283f24fe1245b4d096f2
Shoxrux <sodiqdevpython@gmail.com>
2024-05-20 19:44:50 +0500

    wehdfc

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -8,9 +8,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0'
 
