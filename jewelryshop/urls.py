5f4e280ce218800ddee8569535e3a84955de458f
Sodiq <86997572+sodiqdevpython@users.noreply.github.com>
2024-05-20 19:59:25 +0500

    Update urls.py

--- jewelryshop/urls.py
+++ jewelryshop/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('', include('store.urls')),
 ]
 
-# To display images
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
