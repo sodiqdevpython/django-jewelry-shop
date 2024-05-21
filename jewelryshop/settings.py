2f8fcaed9f8f9b31931218376434f5701eab1206
sodiqdevpython <sodiqdevpython@gmail.com>
2024-05-21 06:15:12 +0500

    commit

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -61,9 +61,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'jewelryshop.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
